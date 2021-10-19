8ce7627751478583ef4890eb732901800529b5bd
apiwatdev <82103342+apiwatdev@users.noreply.github.com>
2021-10-19 22:28:33 +0700

    passing data to view

--- 01-ejs/app.js
+++ 01-ejs/app.js
@@ -1,21 +1,26 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.listen(3000)
+app.listen(3000);
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
-app.get('/blogs/create',(req,res)=>{
-    res.render('create')
-})
+app.get("/", (req, res) => {
+  const blogs = [
+    {title:'Yoshi finds eggs', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
+    {title:'Mario finds star', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
+    {title:'How to defeat bowser', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
+  ]
+  res.render("index", { title: "Home",blogs });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
 
-app.use((req,res)=>{
-    res.status(404).render('404')
-})
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
