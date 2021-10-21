d91d419e0f62cfff41fb481e448e3c7cd168c4ba
apiwatdev <82103342+apiwatdev@users.noreply.github.com>
2021-10-21 21:48:27 +0700

    middelware & static files

--- 01-ejs/app.js
+++ 01-ejs/app.js
@@ -1,19 +1,41 @@
 const express = require("express");
-
+const morgen = require("morgan")
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.listen(3000);
 
+// middelware & static files
+app.use(express.static('public'))
+
+app.use(morgen('dev'))
+
+
+
 app.get("/", (req, res) => {
   const blogs = [
-    {title:'Yoshi finds eggs', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
-    {title:'Mario finds star', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
-    {title:'How to defeat bowser', snippet : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur'},
-  ]
-  res.render("index", { title: "Home",blogs });
+    {
+      title: "Yoshi finds eggs",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur",
+    },
+    {
+      title: "Mario finds star",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti quisquam aspernatur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
+
+
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
